22e52f6549c023fdee3c549e1da8c07c6a238536
Anton <fynjy-666@mail.ru>
2025-03-06 18:03:00 +0300

    Add django config Add app Blog Add app Users Add model Blog, Users.

---
+++ blog/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+from users.models import ModelUser
+
+
+class Blog(models.Model):
+    """ Модель блога """
+    title = models.CharField(max_length=250, verbose_name="Заголовок")
+    topic = models.ForeignKey("Topic", on_delete=models.PROTECT, related_name="topic", verbose_name="Тематика блога")
+    blog_text = models.TextField(verbose_name="Содержимое статьи")
+    image = models.ImageField(upload_to="blog/%Y/%m/%d/", null=True, blank=True, verbose_name="Превью (изображение)")
+    created_at = models.DateField(auto_now_add=True, verbose_name="Дата создания")
+    is_published = models.BooleanField(default=True, verbose_name="Опубликовать")
+    view_count = models.PositiveIntegerField(default=0, verbose_name="Количество просмотров")
+    like = models.PositiveIntegerField(default=0, verbose_name="Количество лайков")
+    owner = models.ForeignKey(
+        ModelUser, on_delete=models.CASCADE, related_name="blog", verbose_name="Владелец"
+    )
+    subscriber = models.ManyToManyField(
+        ModelUser, null=True, blank=True, related_name="blog_subscriber", verbose_name="Подписчик"
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Статью"
+        verbose_name_plural = "Статьи"
+        ordering = ["created_at", ]
+
+
+class Topic(models.Model):
+    """ Модель тематик для блога """
+
+    title = models.CharField(max_length=50, verbose_name="Краткое название тематики")
+    description = models.CharField(max_length=50, verbose_name="Небольшое описание тематики")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Тематика"
+        verbose_name_plural = "Тематика"
+        ordering = ["title", ]
---
+++ users/models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class ModelUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    avatar = models.ImageField(upload_to="avatars/", null=True, blank=True)
+    token = models.CharField(max_length=100, null=True, blank=True, verbose_name="Токен")
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = [
+        "username",
+    ]
+
+    def __str__(self):
+        return f'{self.username}-{self.email}'
+
+    class Meta:
+        verbose_name = "пользователя"
+        verbose_name_plural = "Пользователи"
+        permissions = [
+            ("can_block_user", "Can block user"),
+        ]
+
